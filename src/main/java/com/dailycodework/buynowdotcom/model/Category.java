4a7796b0f62efd09ce3dea0fd5f11631820ab658
skbnsl <bansalsagar13@gmail.com>
2025-06-11 19:34:43 +0530

    working on models

--- src/main/java/com/dailycodework/buynowdotcom/model/Category.java
+++ src/main/java/com/dailycodework/buynowdotcom/model/Category.java
@@ -1,4 +1,26 @@
 package com.dailycodework.buynowdotcom.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/dailycodework/buynowdotcom/model/Image.java
+++ src/main/java/com/dailycodework/buynowdotcom/model/Image.java
@@ -1,4 +1,31 @@
 package com.dailycodework.buynowdotcom.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.grammars.ordering.OrderingParser;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
